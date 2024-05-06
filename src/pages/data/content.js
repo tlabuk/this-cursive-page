9372679aa201f66ddecc31f063a563029d128058
tlabuk <tomek.labuk@gmail.com>
2024-05-06 11:05:51 +0200

    Delete unused data

--- src/pages/data/content.js
+++
@@ -1,6 +0,0 @@
-const homePageContent = [{
-    title: "Discourse rules all.",
-    lead: "A word, <i>logos</i>, is the cornerstone of our reality. We write with words, we speak with them, and we create perceptions with them. Words can change reality in its every aspect known to us. Do you want to embrace their strength?"
-}]
-
-export default homePageContent;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import homePageContent from './data/content.js';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
@@ -8,6 +7,10 @@ import ThemedImage from '@theme/ThemedImage';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 
 function HomepageHeader() {
+  const homePageContent = [{
+    title: "Discourse rules all.",
+    lead: "A word, <i>logos</i>, is the cornerstone of our reality. We write with words, we speak with them, and we create perceptions with them. Words can change reality in its every aspect known to us. Do you want to embrace their strength?"
+  }]
 
   return (
     <header>
