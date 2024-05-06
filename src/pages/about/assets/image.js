ed1d6a1a61c4fb8b4b5f9e373484aee23f43cee0
tlabuk <tomek.labuk@gmail.com>
2024-05-06 10:45:28 +0200

    Fix build

--- src/pages/about/assets/image.js
+++ src/pages/about/assets/image.js
@@ -1,19 +1,4 @@
-import AvatarPNG from "./images/avatar.png";
-import SkillCss from "./images/skill_css.png";
-import SkillFigma from "./images/skill_figma.png";
-import SkillHtml5 from "./images/skill_html5.png";
-import SkillPhotoshop from "./images/skill_photoshop.png";
-import SkillSketch from "./images/skill_sketch.png";
-import SkillTrello from "./images/skill_trello.png";
-import SkillWireframe from "./images/skill_wireframe.png";
-import SkillXd from "./images/skill_xd.png";
-import Udemy from "./images/udemy.png";
-import Coursera from "./images/coursera.png";
-import Google from "./images/google.png";
-import Reading from "./images/reading.png";
-import Travelling from "./images/travelling.png";
-import Blogging from "./images/blogging.png";
-import Gaming from "./images/gaming.png";
+
 
 // export {
 //   AvatarPNG,
