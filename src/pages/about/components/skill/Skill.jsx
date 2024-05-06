47bc3bf9d95357789029a365cbf7c8778f84ba27
tlabuk <tomek.labuk@gmail.com>
2024-05-06 14:12:06 +0200

    Fix icons

--- src/pages/about/components/skill/Skill.jsx
+++ src/pages/about/components/skill/Skill.jsx
@@ -1,58 +1,69 @@
+import React from "react";
 import Title from "../common/Title";
 import PropTypes from "prop-types";
 import styles from "../../assets/sass/style.module.scss";
+import skillCss from "/static/img/about/skill_css.png";
+import skillHtml from "/static/img/about/skill_html5.png";
+import skillFigma from "/static/img/about/skill_figma.png";
+import skillPhotoshop from "/static/img/about/skill_photoshop.png";
+import skillTrello from "/static/img/about/skill_trello.png";
+import skillSketch from "/static/img/about/skill_sketch.png";
+import skillWireframe from "/static/img/about/skill_wireframe.png";
+import skillXd from "/static/img/about/skill_xd.png";
+
+const skillsData = [
+  {
+    id: "skill_1",
+    icon: skillFigma,
+    name: "Figma",
+    description: "UI Design, Protoyping",
+  },
+  {
+    id: "skill_2",
+    icon: skillHtml,
+    name: "HTML5",
+    description: "Hypertext Markup",
+  },
+  {
+    id: "skill_3",
+    icon: skillCss,
+    name: "CSS",
+    description: "Attractive Design & Frameworks",
+  },
+  {
+    id: "skill_4",
+    icon: skillPhotoshop,
+    name: "Adobe Photoshop",
+    description: "Picture Edit & Transformation",
+  },
+  {
+    id: "skill_5",
+    icon: skillSketch,
+    name: "Sketch",
+    description: "UI Design & Prototyping",
+  },
+  {
+    id: "skill_6",
+    icon: skillTrello,
+    name: "Trello",
+    description: "Project Management Knowledge",
+  },
+  {
+    id: "skill_7",
+    icon: skillWireframe,
+    name: "Wireframing",
+    description: "Creating wireframes prior to design",
+  },
+  {
+    id: "skill_8",
+    icon: skillXd,
+    name: "Adobe XD",
+    description: "Adobe Tool for UI Design",
+  },
+];
 
 const Skill = () => {
-  const skillsData = [
-    {
-      id: "skill_1",
-      icon: "/img/about/skill_figma.png",
-      name: "Figma",
-      description: "UI Design, Protoyping",
-    },
-    {
-      id: "skill_2",
-      icon: "/img/about/skill_html5.png",
-      name: "HTML5",
-      description: "Hypertext Markup",
-    },
-    {
-      id: "skill_3",
-      icon: "/img/about/skill_css.png",
-      name: "CSS",
-      description: "Attractive Design & Frameworks",
-    },
-    {
-      id: "skill_4",
-      icon: "/img/about/skill_photoshop.png",
-      name: "Adobe Photoshop",
-      description: "Picture Edit & Transformation",
-    },
-    {
-      id: "skill_5",
-      icon: "/img/about/skill_sketch.png",
-      name: "Sketch",
-      description: "UI Design & Prototyping",
-    },
-    {
-      id: "skill_6",
-      icon: "/img/about/skill_trello.png",
-      name: "Trello",
-      description: "Project Management Knowledge",
-    },
-    {
-      id: "skill_7",
-      icon: "/img/about/skill_wireframe.png",
-      name: "Wireframing",
-      description: "Creating wireframes prior to design",
-    },
-    {
-      id: "skill_8",
-      icon: "/img/about/skill_xd.png",
-      name: "Adobe XD",
-      description: "Adobe Tool for UI Design",
-    },
-  ];
+  // Define skillsData directly inside the component
 
   return (
     <div className={`${styles.skillSc} ${styles.resumeBlock}`}>
@@ -89,12 +100,4 @@ const SkillItem = ({ item }) => {
       </div>
     </div>
   );
-};
-
-SkillItem.propTypes = {
-  item: PropTypes.shape({
-    id: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired,
-  }),
 };
