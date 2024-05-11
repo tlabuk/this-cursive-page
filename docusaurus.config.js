693ce3f41fbd81d8a55e4ac7acef2b10f966c325
tlabuk <tomek.labuk@gmail.com>
2024-05-11 14:18:28 +0200

    Social card config - fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -80,7 +80,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/socialcard.jpg',
+      image: 'img/social-card.jpg',
       navbar: {
         logo: {
           alt: 'Site logo',
